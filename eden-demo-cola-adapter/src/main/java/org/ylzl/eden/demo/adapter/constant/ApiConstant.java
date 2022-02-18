00d17994300560a6a7cbc0e2fab7b811b814521b
shiyindaxiaojie <1813986321@qq.com>
2022-02-19 00:56:10 +0800

    init

--- eden-demo-cola-adapter/src/main/java/org/ylzl/eden/demo/adapter/constant/ApiConstant.java
+++ eden-demo-cola-adapter/src/main/java/org/ylzl/eden/demo/adapter/constant/ApiConstant.java
@@ -1,11 +1,14 @@
 package org.ylzl.eden.demo.adapter.constant;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * API 常量定义
  *
  * @author <a href="mailto:shiyindaxiaojie@gmail.com">gyl</a>
  * @since 2.4.x
  */
+@UtilityClass
 public class ApiConstant {
 
 	public static final String WEB_API_PATH = "/api";
