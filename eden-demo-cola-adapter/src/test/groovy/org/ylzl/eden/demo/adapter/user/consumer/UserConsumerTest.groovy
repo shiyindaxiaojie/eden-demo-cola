690c663fe729c2083effde4275b8e5a49d5623e4
guoyuanlu <guoyuanlu@puyiwm.com>
2022-06-15 20:58:10 +0800

    233

---
+++ eden-demo-cola-adapter/src/test/groovy/org/ylzl/eden/demo/adapter/user/consumer/UserConsumerTest.groovy
@@ -0,0 +1,32 @@
+package org.ylzl.eden.demo.adapter.user.consumer
+
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.slf4j.Logger
+import org.ylzl.eden.spring.integration.messagequeue.core.Message
+import org.ylzl.eden.spring.integration.messagequeue.core.consumer.Acknowledgement
+import spock.lang.Specification
+
+class UserConsumerTest extends Specification {
+    @Mock
+    Logger log
+    @InjectMocks
+    UserConsumer userConsumer
+	@Mock
+	Acknowledgement acknowledgement
+
+    def setup() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    def "test consume"() {
+        when:
+        userConsumer.consume([new Message("namespace", "topic", 0, "key", "tags", "body", 0)], acknowledgement)
+
+        then:
+        true
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
