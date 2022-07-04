43548345898f6de5d0b5bd4c26714604ff1d3fd6
shiyindaxiaojie <1813986321@qq.com>
2022-07-04 23:48:00 +0800

    fixed

--- eden-demo-cola-start/src/main/docker/jib/entrypoint.sh
+++ eden-demo-cola-start/src/main/docker/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start in ${RUN_SLEEP}s..." && sleep ${RUN_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.ylzl.eden.demo.Application" "$@"
+exec java ${JAVA_OPTS} ${ENV_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.ylzl.eden.demo.Application" "$@"
