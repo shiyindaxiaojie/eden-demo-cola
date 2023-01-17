30becf4410d1a0c624542d1c82404614d5c0da65
shiyindaxiaojie <1813986321@qq.com>
2023-01-17 23:47:22 +0800

    fixed

--- eden-demo-cola-start/src/main/java/org/ylzl/eden/demo/ColaApplication.java
+++ eden-demo-cola-start/src/main/java/org/ylzl/eden/demo/ColaApplication.java
@@ -23,7 +23,6 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.ylzl.eden.event.auditor.EnableEventAuditor;
 import org.ylzl.eden.spring.boot.bootstrap.SpringBootApplicationHelper;
 import org.ylzl.eden.spring.framework.web.rest.annotation.EnableRestExceptionResolver;
 
@@ -33,7 +32,6 @@ import org.ylzl.eden.spring.framework.web.rest.annotation.EnableRestExceptionRes
  * @author <a href="mailto:shiyindaxiaojie@gmail.com">gyl</a>
  * @since 2.4.13
  */
-@EnableEventAuditor
 @EnableRestExceptionResolver
 @EnableDiscoveryClient
 @EnableTransactionManagement
