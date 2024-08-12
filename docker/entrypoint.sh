1c2356a0cda238543d0847a1dbc9d2b914feebaa
guoyuanlu <guoyuanlu@puyiwm.com>
2024-08-12 20:10:41 +0800

    optimize jvm argument

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
+JAVA_MAJOR_VERSION=$($JAVA -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
+
 JAVA_OPTS="${JAVA_OPTS} -server"
-JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UnlockCommercialFeatures"
+JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions"
 JAVA_OPTS="${JAVA_OPTS} -XX:+AlwaysPreTouch -XX:+PrintFlagsFinal -XX:-DisplayVMOutput -XX:-OmitStackTraceInFastThrow"
 JAVA_OPTS="${JAVA_OPTS} -Xms${JVM_XMS} -Xmx${JVM_XMX} -Xss${JVM_XSS} -XX:MetaspaceSize=${JVM_MS}"
 
-if [[ "${USE_G1_GC}" == "y" ]]; then
-	echo "Garbage first collector is enabled."
+if [[ "$JAVA_MAJOR_VERSION" -le "8" ]] ; then
+	JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockCommercialFeatures"
+fi
+
+if [[ "${GC_MODE}" == "g1" ]]; then
+	echo "GC mode is G1"
 	JAVA_OPTS="${JAVA_OPTS} -XX:+UseG1GC -XX:G1LogLevel=finest -XX:MaxGCPauseMillis=200"
 	JAVA_OPTS="${JAVA_OPTS} -XX:ConcGCThreads=6 -XX:ParallelGCThreads=8"
 	JAVA_OPTS="${JAVA_OPTS} -XX:InitiatingHeapOccupancyPercent=45 -XX:G1ReservePercent=10 -XX:G1HeapWastePercent=5"
-	JAVA_OPTS="${JAVA_OPTS} -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=60 -XX:G1HeapRegionSize=2m"
+	JAVA_OPTS="${JAVA_OPTS} -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=60"
 	JAVA_OPTS="${JAVA_OPTS} -XX:G1MixedGCCountTarget=8 -XX:G1MixedGCLiveThresholdPercent=65"
 	JAVA_OPTS="${JAVA_OPTS} -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled"
 fi
 
-if [[ "${USE_HEAP_DUMP}" == "y" ]]; then
-	echo "Heap dump path is '${WORKDIR}/logs/jvm_heap_dump.hprof'."
-	JAVA_OPTS="${JAVA_OPTS} -XX:HeapDumpPath=${WORKDIR}/logs/jvm_heap_dump.hprof -XX:+HeapDumpOnOutOfMemoryError"
+if [[ "${USE_GC_LOG}" == "y" ]]; then
+    echo "GC log path is '${HOME}/logs/jvm_gc.log'."
+    JAVA_OPTS="${JAVA_OPTS} -XX:+PrintVMOptions"
+    if [[ "$JAVA_MAJOR_VERSION" -gt "8" ]] ; then
+        JAVA_OPTS="${JAVA_OPTS} -Xlog:gc=${HOME}/logs/jvm_gc.log,gc+datestamps,tags,timer,uptime:filecount=10,filesize=100M"
+	else
+		JAVA_OPTS="${JAVA_OPTS} -Xloggc:${HOME}/logs/jvm_gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps"
+		JAVA_OPTS="${JAVA_OPTS} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
+		JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCCause -XX:+PrintGCApplicationStoppedTime"
+		JAVA_OPTS="${JAVA_OPTS} -XX:+PrintTLAB -XX:+PrintReferenceGC -XX:+PrintHeapAtGC"
+		JAVA_OPTS="${JAVA_OPTS} -XX:+FlightRecorder -XX:+PrintSafepointStatistics -XX:PrintSafepointStatisticsCount=1"
+        JAVA_OPTS="${JAVA_OPTS} -XX:+DebugNonSafepoints -XX:+SafepointTimeout -XX:SafepointTimeoutDelay=500"
+	fi
 fi
 
-if [[ "${USE_GC_LOG}" == "y" ]]; then
-    echo "GC log path is '/dev/shm/logs/jvm_gc.log'."
-    JAVA_OPTS="${JAVA_OPTS} -Xloggc:/dev/shm/logs/jvm_gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
-    JAVA_OPTS="${JAVA_OPTS} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10m"
-    JAVA_OPTS="${JAVA_OPTS} -XX:+PrintVMOptions -XX:+PrintGCCause -XX:+PrintGCApplicationStoppedTime"
-	JAVA_OPTS="${JAVA_OPTS} -XX:+PrintTLAB -XX:+PrintReferenceGC -XX:+PrintHeapAtGC"
-	JAVA_OPTS="${JAVA_OPTS} -XX:+FlightRecorder -XX:+PrintSafepointStatistics -XX:PrintSafepointStatisticsCount=1"
-    JAVA_OPTS="${JAVA_OPTS} -XX:+DebugNonSafepoints -XX:+SafepointTimeout -XX:SafepointTimeoutDelay=500"
+if [ ! -d "${HOME}/logs" ]; then
+  mkdir ${HOME}/logs
+fi
+
+if [[ "${USE_HEAP_DUMP}" == "y" ]]; then
+	echo "Heap dump path is '${HOME}/logs/jvm_heap_dump.hprof'."
+	JAVA_OPTS="${JAVA_OPTS} -XX:HeapDumpPath=${HOME}/logs/jvm_heap_dump.hprof -XX:+HeapDumpOnOutOfMemoryError"
 fi
 
 if [[ "${USE_LARGE_PAGES}" == "y" ]]; then
